f633efac813c2f4321d7c85f45482ce171a8cccc
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-01-24 09:31:48 +0100

    form

--- reacthooks/src/Components/Form/form.jsx
+++ reacthooks/src/Components/Form/form.jsx
@@ -1,67 +1,83 @@
 import { useState } from "react";
 
 const Form = () => {
-  const [contactInfo, setContactInfo] = useState({
-    name: "",
-    phonenumber: "",
-    email: "",
-    comments: "",
-  });
-  const handleChange = (event) => {
-    setContactInfo({ ...contactInfo, [event.target.name]: event.target.value });
-  };
-  const handleSubmit = (event) => {
-    // prevents the submit button from refreshing the page
-    event.preventDefault();
-    console.log(contactInfo);
-    setContactInfo({ name: "", email: "", phonenumber: "", comments: "" });
+  const [formElm, setFormElm] = useState(
+    {},
+    {
+      fullName: "...",
+      phoneNumber: "...",
+      email: "...",
+      comments: "...",
+    }
+  );
+  const registerInput = (e) => {
+    setFormElm((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <h3>Contact Form</h3>
-
-        <input
-          type="text"
-          name="name"
-          placeholder="Fulde navn"
-          value={contactInfo.name}
-          onChange={handleChange}
-        />
-
-        <input
-          type="number"
-          name="phonenumber"
-          placeholder="Telefon nummer"
-          value={contactInfo.phonenumber}
-          onChange={handleChange}
-        />
-
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={contactInfo.email}
-          onChange={handleChange}
-        />
-
-        <textarea
-          name="comments"
-          cols="30"
-          rows="10"
-          placeholder="Skriv dine kommentarer her"
-          value={contactInfo.comments}
-          onChange={handleChange}
-        ></textarea>
-
-        <input type="checkbox" />
-        <input type="checkbox" />
-
-        <button type="reset">Reset</button>
-        <button type="submit">Send</button>
+    <div>
+      <form>
+        <h2>Form elements</h2>
+        <div>
+          <label htmlFor="fullname">Fulde navn</label>
+          <input
+            type="text"
+            name="fullname"
+            id="fullname"
+            onChange={registerInput}
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Telefon nummer</label>
+          <input
+            type="number"
+            name="phone"
+            id="phone"
+            onChange={registerInput}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={registerInput}
+          />
+        </div>
+        <div>
+          <textarea
+            name="comments"
+            id="comments"
+            cols="30"
+            rows="10"
+            placeholder="Kommentarer"
+            onChange={registerInput}
+          ></textarea>
+        </div>
+        <div>
+          <input type="checkbox" name="contact" />{" "}
+          <label htmlFor="">Vil gerne kontaktes med email</label> <br />
+          <input type="checkbox" name="contact" />{" "}
+          <label htmlFor="">Vil gerne kontaktes med telefon</label>
+        </div>
+        <div>
+          <button type="reset">Reset</button>
+          <button type="submit">Send</button>
+        </div>
       </form>
-    </>
+
+      <section>
+        <ul>
+          <li>Fulde navn: {formElm.fullName}</li>
+          <li>Telefon nummer: {formElm.phoneNumber}</li>
+          <li>Email: {formElm.email}</li>
+          <li>Kommentarer: {formElm.comments}</li>
+        </ul>
+      </section>
+    </div>
   );
 };
 
